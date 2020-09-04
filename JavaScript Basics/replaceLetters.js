c38b1a57003cd1cc205e3d2df258fe08001c1be3
Sudarshan Guttula <sidguttula@yahoo.com>
2020-09-04 12:52:07 -0700

    Replaces letters in the string with a dash

---
+++ JavaScript Basics/replaceLetters.js
@@ -0,0 +1,8 @@
+
+var stringInput = "node.js";
+var replaceLetters = stringInput;
+var swap = replaceLetters.replace("a", "-").replace("e", "-").replace("i", "-").replace("o", "-").replace("u", "-");
+
+console.log(stringInput);
+console.log(swap);
+
