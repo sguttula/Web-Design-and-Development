584750ca43193201291e1ef4113ac65f3889ac63
Sudarshan Guttula <sidguttula@yahoo.com>
2020-09-04 12:54:39 -0700

    Object with the key as the word and the count as the value

---
+++ JavaScript Basics/createAnimals.js
@@ -0,0 +1,11 @@
+
+const arr =[
+            [{ property: 'name', assign: 'Garfield'},
+             { property: 'owner', assign: 'Jon Arbuckle'},
+             { property: 'type', assign: 'cat' }],
+            [{ property: 'name',assign: 'Snoopy' },
+             { property: 'owner',assign: 'Charlie Brown' },
+             { property: 'type',assign: 'dog' }]
+            ]           
+                 console.log("0:", arr[0][0], arr[0][1], arr[0][2]);
+                 console.log("1:", arr[1][0], arr[1][1], arr[1][2]);
